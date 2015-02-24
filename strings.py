7619a7e411bc3430e8556f660972460ab4cc577f
troywill1 <troywill1@gmail.com>
2015-02-24 16:05:15 -0600

    String Practice
    Added additional string examples from Stage 2: Work Session 2.

--- strings.py
+++ strings.py
@@ -45,3 +45,41 @@ print word[4:6]	# print from position 4 to 5
 print word[4:]	# print from position 4 to the end
 print word[:2]	# print from the beginning to position 1
 print word[:]	# print from the beginning to the end
+
+# Stage 2: Work Session 2 - Strings and Variables Demo 1
+
+# Are you wondering why there's two ways to make strings
+# (single quotes AND double quotes)??
+
+# Look at the code below. How would we have written that
+# if the only Python string character were the double quote?
+
+div_with_class = '<div class="concept-description">'
+just_the_class = div_with_class[5:-1]
+print just_the_class
+
+# Stage 2: Work Session 2 - Strings and Variables Practice 2
+
+# In addition to using singel quotes (') or double quotes (")
+# to create a string, you can also use TRIPLE quotes to create
+# a multi-line string.
+
+print """I am a string
+that takes up
+multiple lines"""
+
+print '''I am also a string
+that takes up multiple lines!'''
+
+print '''
+<div class="concept">
+	<div class="concept-title">
+		Multi-line strings
+	</div>
+	<div class="concept-description">
+		Multi-line strings can be made with triple single
+		quotes or triple double quotes. But I'm not sure
+		why they are useful yet.
+	</div>
+</div>
+'''
