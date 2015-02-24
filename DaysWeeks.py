693f35d27a3a14084182911324b8b731fd154df1
troywill1 <troy@Troy-Williamss-Mac-mini.local>
2015-02-23 19:30:00 -0600

    Stage 2: Work Session 2
    Added two Python files during exercises in the work session.

---
+++ DaysWeeks.py
@@ -0,0 +1,8 @@
+# Write Python code that prints out the number of hours in 7 weeks.
+
+hours_per_day = 24
+days_per_week = 7
+
+hours_seven_weeks = (hours_per_day * days_per_week) * 7
+
+print "Number of hours in 7 weeks: " + str(hours_seven_weeks)
---
+++ bodacious.py
@@ -0,0 +1,12 @@
+# Given the variables s and t defined as:
+s = 'udacity'
+t = 'bodacious'
+# write Python code that prints out udacious
+# without using any quote characters in
+# your code.
+
+print s[0:2] + t[3:]
+
+# -or-
+
+print s[:-2] + t[-3:]
