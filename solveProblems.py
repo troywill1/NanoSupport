d4192872c77ca80cd35fb3b43d47bf55be4c7e89
troywill1 <troywill1@gmail.com>
2016-01-12 15:59:55 -0600

    Coded Function
    Coded the quiz function from the “How to Solve Problems” lesson.

--- solveProblems.py
+++ solveProblems.py
@@ -7,11 +7,44 @@
 ###    nextDay(2013, 1, 30) => (2013, 2, 1)
 ###    nextDay(2012, 12, 30) => (2013, 1, 1)  (even though December really has 31 days)
 ###
- 
+
 def nextDay(year, month, day):
     """
     Returns the year, month, day of the next day.
     Simple version: assume every month has 30 days.
     """
     # YOUR CODE HERE
-    return
+    if day != 30:
+
+        day += 1
+
+        if day == 31:
+            day = 1
+        return (year, month, day)
+
+    elif month != 12:
+
+        day += 1
+        month += 1
+
+        if day == 31:
+            day = 1
+        if month == 13:
+            month = 1
+        return (year, month, day)
+
+    day += 1
+    month += 1
+
+    if day == 31:
+        day = 1
+    if month == 13:
+        month = 1
+
+    return(year + 1, month, day)
+
+# Test the function
+
+print nextDay(1999, 12, 30)
+print nextDay(2013, 1, 30)
+print nextDay(2012, 12, 30)
