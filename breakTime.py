b5eee676a8bb6cd9028336a5e34f06325881c614
troywill1 <troywill1@gmail.com>
2016-01-27 15:46:26 -0600

    Lesson 3.2 Take a Break program
    Added stub file for the "Take a Break" mini-project.

---
+++ breakTime.py
@@ -0,0 +1,13 @@
+# Lesson 3.2: Use functions
+# Mini-Project: Take a Break
+
+# Write a program that prompts the user to take a break
+# once every two hours, a maximum of three times.
+
+# User this space to describe your approach to the problem.
+# 
+#
+#
+#
+
+# Your code here.
