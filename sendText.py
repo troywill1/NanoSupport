8b7c6209bca0db34ea008a0cd50ff70a4f4617b7
troywill1 <troywill1@gmail.com>
2016-02-26 15:40:09 -0600

    Lesson 3.3a Use Classes - Draw Turtles
    Added a new file fractalProj.py as a mini-project to finish out Lesson
    3.3a. Began Lesson 3.3b by adding file sendText.py.

---
+++ sendText.py
@@ -0,0 +1,10 @@
+# Lesson 3.3b: Use Classes
+# Mini-Project: Send Text
+
+# It can be important for businesses to automate sending text messages. In
+# this mini-project we'll use classes to send a text message using Twilio,
+# a library we'll download from the Internet and add to Python.
+
+from twilio.rest import TwilioRestClient
+
+# Your code here.
